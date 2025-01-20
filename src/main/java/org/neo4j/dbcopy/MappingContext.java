4e338d63befebbb85f9fd50913b0ed62dc623a24
Nicolas Mervaillie <nmervaillie@gmail.com>
2025-01-20 16:44:40 +0100

    Fix concurrency issue in source to target DB mapping structure

--- src/main/java/org/neo4j/dbcopy/MappingContext.java
+++ src/main/java/org/neo4j/dbcopy/MappingContext.java
@@ -1,16 +1,17 @@
 package org.neo4j.dbcopy;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MappingContext {
 
     record Mapping(long sourceNodeId, long targetNodeId){}
 
-    private static HashMap<Long, Long> MAPPINGS;
+    private static Map<Long, Long> MAPPINGS;
 
     public MappingContext(int initialCapacity) {
-        MAPPINGS = new HashMap<>(initialCapacity);
+        MAPPINGS = new ConcurrentHashMap<>(initialCapacity);
     }
 
     MappingContext add(List<Mapping> mappings) {
